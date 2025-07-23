186ece49ee1fda6092a4b00e2fb0c91dc7448665
Vikas Patel <vikaspatel07071999@gmail.com>
2025-07-23 13:29:16 +0530

    js basics

---
+++ Lect-15-js basics/test.js
@@ -0,0 +1,72 @@
+// var x;
+// let var const 
+
+
+// let x=90;
+// x=9;
+
+// if(x>10){  
+//       console.log("x is greater than 10");
+
+// }
+// else{
+//     console.log("x is less than 10");
+    
+// }
+// let str="vikas patel";
+// console.log(str);
+
+// let ans=null;
+// console.log(ans);
+
+// let present =true;
+// console.log(present);
+// console.log(typeof(ans));
+
+
+// let arr=[23,45,23,11,55,"vikas",true,2.355,undefined,null];
+// console.log(arr[8]);
+
+
+
+
+// let x=[653,"dgf","ew",true];
+// let x;
+// console.log(x);
+// console.log(typeof(x));
+// console.log(typeof(x[1]));
+
+
+// console.log(obj["mark"]);
+// console.log(obj);
+
+// let x='A';
+// console.log(x);
+// console.log(typeof(x));
+
+
+// let x=
+// function add(a,b){
+//     return a+b;
+// };
+// console.log(x);
+// console.log(typeof(x));
+
+
+
+// let ans=add(2,4);
+// console.log(ans);
+
+const x=90;
+x=78;
+x="vikas";
+x=true;
+
+
+
+
+
+
+
+
+
