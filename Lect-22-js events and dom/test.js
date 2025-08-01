868890a6c790a0528818c890f830c45d2939e7ab
Vikas Patel <vikaspatel07071999@gmail.com>
2025-08-01 13:28:39 +0530

    calculator project

---
+++ Lect-22-js events and dom/test.js
@@ -0,0 +1,43 @@
+// console.log(document);
+
+// console.log(window);
+
+// window.document.getElementById("")
+
+
+
+// alert("form submitted");
+// window.alert("thank you")
+// if (confirm("Press a button!")) {
+//   txt = "You pressed OK!";
+// } else {
+//   txt = "You pressed Cancel!";
+// }
+
+
+// let person = prompt("Please enter your name", "Harry Potter");
+// let text;
+// if (person == null || person == "") {
+//   text = "User cancelled the prompt.";
+// } else {
+//   text = "Hello " + person + "! How are you today?";
+// }
+
+// console.log(text);
+
+// console.log(window);
+
+// var a=90;
+// console.log(a);
+// console.log(window.a);
+
+// function display(){
+//     window.open("https://www.w3schools.com");
+// }
+
+// var a=undefined;
+
+// console.log(a);
+// var a=90;
+
+
