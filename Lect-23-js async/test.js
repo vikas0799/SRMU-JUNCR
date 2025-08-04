bf278ed81a9c68c23cfe7dcbc3c0d902513e857f
Vikas Patel <vikaspatel07071999@gmail.com>
2025-08-04 13:35:20 +0530

    event loop

---
+++ Lect-23-js async/test.js
@@ -0,0 +1,37 @@
+// console.log("hello");
+
+// let settimeoutID=setTimeout(() => {
+//     console.log("callback execution in settimout");
+//     console.log("take 4 second to execute"); 
+// }, 4000);
+
+// // clearTimeout(settimeoutID);
+
+
+// let setintervalID=setInterval(() => {
+//     console.log("setimeinterval callback");
+//     console.log("callback function executes every 3s interval");
+    
+// }, 3000);
+
+// // clearInterval(setintervalID);
+// console.log("patel");
+
+
+// console.log("arjun");
+
+// setTimeout(() => {
+//   console.log("shivam");
+// }, 3000);
+
+// setTimeout(() => {
+//   console.log("akash");
+    
+// }, 2000);
+
+// console.log("vikas");
+
+// console.log("hello");
+
+// fetch("httphbhzh.com")
+// console.log("bye");
