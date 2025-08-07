6dfd3cf6d569303efa61d48032799d304ad96f0d
Vikas Patel <vikaspatel07071999@gmail.com>
2025-08-07 13:40:08 +0530

    js proto

---
+++ Lect-26-API calls/prototype/test.js
@@ -0,0 +1,74 @@
+// // let a=Object;
+// // let a=Object();
+// // console.log(a);
+
+
+// // let a=90;  //primitive data
+// // let b=new Number(100);   //object number 
+
+//  //
+
+// // console.log(a);
+// // console.log(b);
+
+// // console.log(typeof(a));
+// // console.log(typeof(b));
+
+// // let c=Number(20);
+
+
+
+// // console.log(c.__proto__.__proto__.__proto__);
+
+// let akash={
+//     "name":"aksh srivastava",
+//     "roll":35,
+//     "age":26
+// };
+
+// console.log(akash);
+// // console.log(akash.__proto__);
+
+// // console.log(akash.__proto__.__proto__);
+
+
+// akash.hometown="gorakhpur";
+// console.log(akash);
+
+
+// //dirty, key value pair in Object Prototype
+// akash.__proto__.RBU="mohali";
+// akash.__proto__.SRMU="lucknow";
+
+
+// let a=Object();
+// console.log(a);
+
+
+// let s=new String("vikas patel");
+// console.log(s);
+// console.log(s.__proto__);
+// console.log(s.__proto__.__proto__);
+// console.log(s.__proto__.__proto__.RBU);
+// console.log(s.RBU);
+
+
+
+
+
+let a=90;
+console.log(a);
+
+
+let akash={
+    "name":"aksh srivastava",
+    "roll":35,
+    "age":26
+};
+console.log(akash);
+
+
+
+
+
+
